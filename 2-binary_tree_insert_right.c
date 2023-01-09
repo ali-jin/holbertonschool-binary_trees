0d178c4dac4b92451047a983600a6b636b970b5a
rChrisb <5767@holbertonstudents.com>
2023-01-09 16:54:08 +0100

    Write a function that inserts a node as the right-child of another node

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	if (parent == NULL)
 		return (NULL);
-	
+
 	new_child = malloc(sizeof(binary_tree_t));
 
 	if (new_child == NULL)
@@ -33,6 +33,6 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		parent->right = new_child;
 	}
 
-	
+
 	return (new_child);
 }
