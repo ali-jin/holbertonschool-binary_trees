ff9f03ea8a76c6685d1db695f924380406fc885f
root <root@pc>
2023-01-09 17:13:58 +0100

    add if condition NewNode == NULL

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *NewNode = malloc(sizeof(binary_tree_t));
 
-	if (parent == NULL)
+	if (parent == NULL || NewNode == NULL)
 		return (NULL);
 
 	NewNode->n = value;
